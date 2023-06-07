26df2cab2614fa641ab37ae152fcd66e7007609a
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-06-07 16:17:04 +0000

    AsyncTask

--- app/src/main/java/com/example/m213_ch09_tp01_trainnes/MainActivity.java
+++ app/src/main/java/com/example/m213_ch09_tp01_trainnes/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.m213_ch09_tp01_trainnes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.example.m213_ch09_tp01_trainnes.databinding.ActivityMainBinding;
 
+import java.security.Permission;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -23,34 +24,45 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
+        TimeAsyncTask timeAsyncTask = new TimeAsyncTask();
+        timeAsyncTask.execute(1);
+
         binding.btnQuit.setOnClickListener(v -> {
             finish();
         });
-
-        Thread thread = new Thread() { // Background Thread
-            @Override
-            public void run() {
-                do {
-                    runOnUiThread(() -> getTime());
-
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                }while (true);
-            }
-        };
-
-        thread.start();
-
-
     }
 
     private void getTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
         binding.tvTime.setText(sdf.format(new Date()));
 //        Log.i("TAG", sdf.format(new Date()));
+    }
+
+    class TimeAsyncTask extends AsyncTask<Integer, Integer, Void> {
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            int i = 0;
+            do {
+                publishProgress(i);
+
+                try {
+                    Thread.sleep(integers[0]);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                i++;
+            } while (true);
+
+//            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... integers) {
+            super.onProgressUpdate(integers);
+
+//            getTime();
+            binding.tvTime.setText(Integer.toString(integers[0]));
+        }
     }
 }
