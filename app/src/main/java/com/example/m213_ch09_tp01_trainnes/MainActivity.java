a5c70a03ed09da440aa3d67959f6f5e416049fa6
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-06-07 15:46:14 +0000

    Thread

--- app/src/main/java/com/example/m213_ch09_tp01_trainnes/MainActivity.java
+++ app/src/main/java/com/example/m213_ch09_tp01_trainnes/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.m213_ch09_tp01_trainnes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.m213_ch09_tp01_trainnes.databinding.ActivityMainBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.btnQuit.setOnClickListener(v -> {
+            finish();
+        });
+
+        Thread thread = new Thread() { // Background Thread
+            @Override
+            public void run() {
+                do {
+                    runOnUiThread(() -> getTime());
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }while (true);
+            }
+        };
+
+        thread.start();
+
+
+    }
+
+    private void getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+        binding.tvTime.setText(sdf.format(new Date()));
+//        Log.i("TAG", sdf.format(new Date()));
     }
 }
