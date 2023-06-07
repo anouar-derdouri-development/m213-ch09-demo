d6e415fa34ef668061f30069302a8b4073d21b50
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-06-07 14:34:20 +0000

    Initial commit

---
+++ app/src/main/java/com/example/m213_ch09_tp01_trainnes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.m213_ch09_tp01_trainnes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
